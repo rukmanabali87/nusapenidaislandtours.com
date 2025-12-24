6451689a5db90ff7e3beed840aa8bc1bf598d28c
Agus Rukmana <rukmanabali87@gmail.com>
2025-12-24 22:49:02 +0800

    coba dynamaic semua

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,15 @@ import Image from "next/image";
 import Link from "next/link";
 import dynamic from "next/dynamic"; 
 
-import Tagline from "./components/tagline";
-import Navbar from "./components/navbar";
+// import Tagline from "./components/tagline";
+// import Navbar from "./components/navbar";
 import VideoModalOne from "./components/video-modal-one";
-import Form from "./components/form";
+// import Form from "./components/form";
 // import TopDestinationOne from "./components/top-destination-one";
-import About from "./components/about";
+// import About from "./components/about";
 
-import Footer from "./components/footer";
-import Switcher from "./components/switcher";
+// import Footer from "./components/footer";
+// import Switcher from "./components/switcher";
 
 
 
@@ -20,9 +20,17 @@ import { packages } from './data/data'
 import { FiMapPin } from 'react-icons/fi'
 
 // Dynamic import untuk komponen non-kritis
+const Tagline = dynamic(() => import('./components/tagline'), { ssr: false });
+const Navbar = dynamic(() => import('./components/navbar'), { ssr: false });
+
+const Form = dynamic(() => import('./components/form'), { ssr: false });
 const TopDestinationOne = dynamic(() => import('./components/top-destination-one'), { ssr: false });
 const Blogs = dynamic(() => import('./components/blogs'), { ssr: false });
 const Client = dynamic(() => import('./components/client'), { ssr: false });
+const About = dynamic(() => import('./components/about'), { ssr: false });
+const Footer = dynamic(() => import('./components/footer'), { ssr: false });
+const Switcher = dynamic(() => import('./components/switcher'), { ssr: false });
+
 
 export default function Home() {
     return (
