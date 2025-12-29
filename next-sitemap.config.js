24309cc3c19230fc9eb83b43f85072d2e76f5655
agusrukmana <agusrukmana87@gmail.com>
2025-12-29 11:18:54 +0800

    up next js sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.nusapenidaislandtours.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+}
