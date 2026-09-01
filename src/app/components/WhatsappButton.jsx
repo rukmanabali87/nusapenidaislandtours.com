47aedaa369dfc52040fefab57b992dbab215a1e7
rukmanabali87 <herakkes04@gmail.com>
2026-09-01 15:45:31 +0800

    mengubah no whatsapp button

--- src/app/components/WhatsappButton.jsx
+++ src/app/components/WhatsappButton.jsx
@@ -1,7 +1,7 @@
 import { FaWhatsapp } from "react-icons/fa";
 
 const WhatsappButton = () => {
-  const phoneNumber = "628123456789";
+  const phoneNumber = "6282233083081";
   const message = "Halo, saya ingin bertanya tentang layanan Anda!";
   const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
